34da6fb8a0b4c24b6a91770a1bbc379a4edd105a
vinay1723 <vinaykumar.17july@gmail.com>
2026-02-03 23:11:20 +0530

    third commit

--- app.js
+++ app.js
@@ -6,22 +6,14 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://pastebin-teal.vercel.app"],
+    origin: "https://pastebin-teal.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
   })
 );
 
-app.options("*", cors());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://pastebin-teal.vercel.app");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,DELETE,PATCH,OPTIONS"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
+app.options("*", (req, res) => {
+  res.sendStatus(200);
 });
 
 app.use(express.json());
