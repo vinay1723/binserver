f11201b486805ba9cf67c32132aba0a46669d4df
vinay1723 <vinaykumar.17july@gmail.com>
2026-02-03 22:55:41 +0530

    third commit

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ import dotenv from "dotenv";
 import app from "./app.js";
 
 dotenv.config({ path: "./config.env" });
+
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB).then((con) => {
-  console.log("Database connected successfully");
-});
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`server is running on ${port}`);
-});
+const startServer = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log("Database connected successfully ✅");
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("MongoDB connection failed ❌", error);
+  }
+};
+
+startServer();
