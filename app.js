f11ffe54242fa641542f3276f6f09bd7f7ea30f8
vinay1723 <vinaykumar.17july@gmail.com>
2026-02-03 23:56:48 +0530

    third commit

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import pasteRoute from "./routes/pasteRoute.js";
 
 const app = express();
+
+dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then((con) => {
+  console.log("Database connected successfully");
+});
 
 app.use(
   cors({
@@ -15,5 +27,11 @@ app.use(
 app.use(express.json());
 
 app.use("/api", pasteRoute);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
 
 export default app;
